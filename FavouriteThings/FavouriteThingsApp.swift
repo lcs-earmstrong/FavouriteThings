24daf2d42e600e55e39fc59adec960fae8fe87ed
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-04 09:48:16 -0500

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Evan Armstrong on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
