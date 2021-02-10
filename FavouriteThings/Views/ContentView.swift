6c9deebe2d290efbe439a665d2f0501723b32cc8
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-10 16:35:41 -0500

    Added all my photos

---
+++ FavouriteThings/Views/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  FavouriteThings
+//
+//  Created by Evan Armstrong on 2021-02-04.
+//
+
+import SwiftUI
+
+// conforms to the view protocol
+// A view is just a structure that is part of the user interface - what is seen and manipulated by the user
+// Each structure that conforms to the view protocol must follow two rules
+// Must have a body property and must 
+struct ContentView: View {
+    
+    // create a copy of the data that we created in Thing.swift
+    // A "store" in developer speak is just "a place that we keep data"
+    var body: some View {
+        
+        NavigationView {
+            
+            List(favouriteThings) { thing in
+
+                  NavigationLink(destination: ThingDetail(heroImage: thing.heroImage,
+                                                          details: thing.details,
+                                                          title: thing.title)) {
+                      
+                      ListItem(hint: thing.hint,
+                               title: thing.title,
+                               summary: thing.summary)
+                      
+                  }
+                  
+
+              }
+              .navigationTitle("Favourite Things")      }
+                }
+            }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+            
+            
+    }
+}
+
